09a412a7f1ca3b186fc48693dcef95f8579dcd60
Takahiro Katsumata <alex.tfkd@gmail.com>
2025-12-26 16:08:18 +0000

    small fix

---
+++ .maketest.sh
@@ -0,0 +1,20 @@
+cd ./ex00
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex01
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex02
+make cpplint
+make
+make fclean
+cd ..
+cd ./ex03
+make cpplint
+make
+make fclean
+cd ..
